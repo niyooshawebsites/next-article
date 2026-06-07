6c76755e4425f13424d8f17e68cf6d5c8e162eca
niyooshawebsites <niyooshawebsites@gmail.com>
2026-06-07 19:14:46 +0530

    create article v1

---
+++ app/(dashboard)/dashboard/article/create/CreateArticleForm.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export default function CreateArticleForm() {
+  return <div>CreateArticleForm</div>;
+}
