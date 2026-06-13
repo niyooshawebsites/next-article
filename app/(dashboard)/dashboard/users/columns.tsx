dec655371c41d2a92ab6457129361e3ef2cdd570
niyooshawebsites <niyooshawebsites@gmail.com>
2026-06-14 00:51:05 +0530

    user table v1

--- app/(dashboard)/dashboard/users/columns.tsx
+++ app/(dashboard)/dashboard/users/columns.tsx
@@ -1,2 +1,35 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
+import UserActions from "./UserActions";
+
+interface User {
+  id: string;
+  email: string;
+  createdAt: Date;
+}
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created At",
+    cell: ({ row }) => {
+      new Date(row.original.createdAt).toLocaleString("en-IN");
+    },
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const user = row.original;
+      return <UserActions userId={user.id} />;
+    },
+  },
+];
--- app/(dashboard)/dashboard/users/user-table.tsx
+++ app/(dashboard)/dashboard/users/user-table.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 
 export default function UserTable() {
   return <div>Users Table</div>;
